61bdbbaa38b5696a2611d26f463ea3d40343f9d4
miguelroman1 <23308060610314@cetis61.edu.mx>
2025-11-11 13:48:54 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import requests
 
+app = Flask(__name__)
+app.secret_key = "clave_super_segura"
+API = "https://pokeapi.co/api/v2/pokemon/"
+
+@app.route('/search', methods=['POST'])
+def busca_pokemon():
+    pokemon_name = request.get('pokemon_name','').strip().lower()
+
+    if not pokemon_name:
+        flash('Por favor, ingresa un nombre de un pokemon')
+        return redirect(url_for('index'))
+    
+@app.route("/")
+def home():
+    return redirect(url_for("inicio")) 
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
